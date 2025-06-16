e53094a8d6e88011b4dea9580228f4b798258c3a
Tferrel8 <tferre3@Zukratzesty.localdomain>
2025-06-16 08:25:25 -0400

    Initial commit of updated HRTV folder

---
+++ HRTV Github/Path Sampling Model Selection/L Segment/Bayesian evaluation of temporal signal/ORC/Log Normal_Popsize_Prior/isochronous/Results/steppt4_final_lognormal_iso/run0.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Set paths
+set ROOT_DIR=C:\tmp_L_het\rln\steppt4_final_lognormal_iso
+set JAVA_EXE="C:\Users\tysha\OneDrive\Desktop\Phylodynamic programs\BEAST.v2.7.6.Windows\BEAST\jre\bin\java.exe"
+set JAR_PATH="C:/Users/tysha/OneDrive/Desktop/Phylodynamic programs/BEAST.v2.7.6.Windows/BEAST/lib/launcher.jar"
+set SEED=103316267190152289
+
+REM Run step 0 normally
+cd "%ROOT_DIR%\step0"
+%JAVA_EXE% -cp %JAR_PATH% beast.pkgmgmt.launcher.BeastLauncher -overwrite -seed %SEED% beast.xml
+
+REM Loop through steps 1 to 199
+for /L %%i in (1,1,199) do (
+    echo Running step %%i...
+
+    set /A prev=%%i-1
+    set PREV_DIR=%ROOT_DIR%\step!prev!
+    set CUR_DIR=%ROOT_DIR%\step%%i
+
+    copy "!PREV_DIR!\beast.xml.state" "!CUR_DIR!" >nul
+
+    cd "!CUR_DIR!"
+    %JAVA_EXE% -cp %JAR_PATH% beast.pkgmgmt.launcher.BeastLauncher -resume -seed %SEED% beast.xml
+)
+
+echo All 200 steps completed.
+pause
+
+    
+        
+        
+
+    
+    
