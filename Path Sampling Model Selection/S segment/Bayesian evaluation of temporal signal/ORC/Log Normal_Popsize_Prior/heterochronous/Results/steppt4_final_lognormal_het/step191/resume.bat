f894da3c457d053a88c2f2353c47e7cb03d7940f
Tferrel8 <tferre3@Zukratzesty.localdomain>
2025-06-22 02:59:25 -0400

    Add S Segment BETs ORC

---
+++ Path Sampling Model Selection/S segment/Bayesian evaluation of temporal signal/ORC/Log Normal_Popsize_Prior/heterochronous/Results/steppt4_final_lognormal_het/step191/resume.bat
@@ -0,0 +1,16 @@
+
+            
+        
+            
+        cd "C:\tmp_S_het\rln\steppt4_final_lognormal_het\step191"
+
+"C:UserstyshaOneDriveDesktopPhylodynamic programsBEAST.v2.7.6.WindowsBEAST./jre/bin/java.exe" -cp "C:/Users/tysha/OneDrive/Desktop/Phylodynamic programs/BEAST.v2.7.6.Windows/BEAST/./lib/launcher.jar" beast.pkgmgmt.launcher.BeastLauncher -resume -seed -460118279994582378 beast.xml
+
+
+
+    
+        
+        
+
+    
+    
